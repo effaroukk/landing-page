1d7d6ebec6f08504f7a8643cf5ec19705769b5b8
Umar Farouk Abdulai <138718476+effaroukk@users.noreply.github.com>
2024-09-12 00:58:50 +0000

    Create Home.js

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h2>Welcome to My Digital Diary</h2>
+    </div>
+  );
+}
+
+export default Home;
