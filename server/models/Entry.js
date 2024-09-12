a3ff9b70719f7f939cf8b960ab7e2691a2795ba7
Umar Farouk Abdulai <138718476+effaroukk@users.noreply.github.com>
2024-09-12 01:19:16 +0000

    Create Entry.js

---
+++ server/models/Entry.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const EntrySchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  mood: { type: String },
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Entry', EntrySchema);
